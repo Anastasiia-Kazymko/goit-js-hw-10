406f45d276564fc3298a7d01ad27baad2ab6d8b9
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-05-16 18:56:02 +0300

    push fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
+const refs = {
+    searchBox: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('country-info'),
+}
 const DEBOUNCE_DELAY = 300;
+
+refs.searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DELAY));
+
+function onSearchBoxInput(e) {
+    let countrySearch = e.target.value;
+
+    fetch(`https://restcountries.com/v3.1/name/${countrySearch}?fields=name,capital,population,flags,languages`).then(
+    (response) => {
+        if (response.ok) {
+            return response.json();
+        }
+    }).then((data) => {
+        const countries = data.reduce((acc, country) => acc + `<li class="country-el"><img src="${country.flags.svg}" width="40"> ${country.name.common}</li>`, "");
+        refs.countryList.insertAdjacentHTML('beforeend', countries);
+        
+    }); 
+}
+
