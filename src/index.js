691a4dc3cf5254592c4524ec14efb84ab1d59841
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-05-17 20:06:13 +0300

    push hw

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const refs = {
     searchBox: document.querySelector('#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('country-info'),
-}
+};
 const DEBOUNCE_DELAY = 300;
 
 refs.searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DELAY));
@@ -14,14 +15,29 @@ function onSearchBoxInput(e) {
     let countrySearch = e.target.value;
 
     fetch(`https://restcountries.com/v3.1/name/${countrySearch}?fields=name,capital,population,flags,languages`).then(
-    (response) => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then((data) => {
-        const countries = data.reduce((acc, country) => acc + `<li class="country-el"><img src="${country.flags.svg}" width="40"> ${country.name.common}</li>`, "");
-        refs.countryList.insertAdjacentHTML('beforeend', countries);
-        
-    }); 
-}
+        (response) => {            
+            if (response.ok) {
+                return response.json();
+            };
+        }).then(showCountry)
+        .catch(error => {
+            console.log(error);
+            Notiflix.Notify.failure(`Oops, there is no country with that name`); 
+        });
+};
 
+
+function showCountry(data) {    
+    if ( data.length > 2 && data.length < 10) {
+            const countries = data.map((country) => `<li class="country-el"><img src="${country.flags.svg}" width="40"> ${country.name.common}</li>`).join("");
+        refs.countryList.insertAdjacentHTML('beforeend', countries);     
+        }
+    if (data.length === 1) {
+            const countryCard = data.map((country) => `<div><div><img src="${country.flags.svg}" width="40"><h1> ${country.name.common}</h1></div><p>Capital: ${country.capital}</p><p>Population: ${country.population}/p><p>Languages: ${country.languages}</p></div>`).join("");
+            console.log(countryCard)
+        refs.countryInfo.insertAdjacentHTML('beforeend', countryCard);
+        }
+        if (data.length > 10) {
+            Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`); 
+        }
+};
