d0994ca68cbe7fcf6f75362b085f8a159a8d9a4d
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-05-17 23:30:58 +0300

    push HW

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ const DEBOUNCE_DELAY = 300;
 
 refs.searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DELAY));
 
+
+
 function onSearchBoxInput(e) {
-    let countrySearch = e.target.value;
+    let countrySearch = e.target.value.trim();
     if (countrySearch === '') {
         refs.countryList.innerHTML = '';
         refs.countryInfo.innerHTML = '';
@@ -24,18 +26,20 @@ function onSearchBoxInput(e) {
 };
 
 function showCountry(data) {    
-    if ( data.length > 2 && data.length < 10) {
+    if ( data.length >= 2 && data.length <= 10) {
             const countries = data.map((country) => `<li class="country-el"><img src="${country.flags.svg}" width="40"> ${country.name.official}</li>`).join("");
-        refs.countryList.insertAdjacentHTML('beforeend', countries);     
+        refs.countryList.insertAdjacentHTML('beforeend', countries);  
+        refs.countryInfo.innerHTML = '';
     };
     if (data.length === 1) {
         const countryCard = data.map((country) => `<div class="card"><div class="title"><img src="${country.flags.svg}" width="40" height="30">
             <h1> ${country.name.official}</h1></div><p><span>Capital:</span> ${country.capital}</p><p><span>Population:</span> ${country.population}</p>
-            <p><span>Languages:</span> ${country.languages.hasOwnProperty(i)}</p></div>`).join("");
+            <p><span>Languages:</span> ${Object.values(country.languages).join(', ')}</p></div>`).join("");
         console.log(countryCard);
-            refs.countryInfo.insertAdjacentHTML('afterbegin', countryCard); 
+        refs.countryInfo.insertAdjacentHTML('afterbegin', countryCard); 
+        refs.countryList.innerHTML = '';
     };
-        if (data.length > 10) {
+    if (data.length > 10) {            
             Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`); 
     };
 };
